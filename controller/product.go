dc3b36b03072ee9e65a2187310a92e4a386c8d02
YusronAbi <yusronabimuliabakhtiar@gmail.com>
2025-01-05 11:13:14 +0700

    add

---
+++ controller/product.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"inventory_sales/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateProduct(c *gin.Context) {
+	var product models.Product
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := models.CreateProduct(&product); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, product)
+}
+
+func GetAllProducts(c *gin.Context) {
+	products, err := models.GetAllProducts()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, products)
+}
